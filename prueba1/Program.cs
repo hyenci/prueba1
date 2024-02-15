2c73a07f20f750d91d825c091629910d7513a4f2
hyenci21 <159926659+hyenci21@users.noreply.github.com>
2024-02-14 19:19:55 -0600

    CODIGO DEFINITIVO

--- prueba1/Program.cs
+++ prueba1/Program.cs
@@ -4,15 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Solicitar al usuario que ingrese un número entero
-        Console.Write("Ingrese un número entero: ");
+   
+       Console.Write("INGRESE UN NUMERO ENTERO");
         int numeroEntero = Convert.ToInt32(Console.ReadLine());
 
-        // Convertir el número entero a decimal
         decimal numeroDecimal = Convert.ToDecimal(numeroEntero);
 
-        // Mostrar el resultado
-        Console.WriteLine($"El número {numeroEntero} en decimal es: {numeroDecimal}");
+  
+        Console.WriteLine($"EL RESULTADO ES: {numeroDecimal}");
     }
 }
-
